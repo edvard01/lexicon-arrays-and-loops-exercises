a66321ff72d84f479e172cbdcf332896a48bdb44
edvard01 <frejaclueless@gmail.com>
2024-01-16 16:36:34 +0100

    Added index files add did the first 6 exercises

---
+++ js/index.js
@@ -0,0 +1,68 @@
+function exercise1() {
+  const arr = [];
+  for (let i = 0; i < 6; i++) {
+    arr.push(i * 3 + 1);
+  }
+
+  return arr;
+}
+
+const array = exercise1();
+
+function exercise2() {
+  console.log("-----------------------------");
+  const arr = [1, 4, 12, 55, 22, 1];
+
+  arr.forEach((element) => {
+    console.log(element * 3);
+  });
+}
+
+function exercise3() {
+  console.log("-----------------------------");
+  const fullArr = [1, 4, 12, 55, 22, 1];
+  const emptyArr = [];
+
+  fullArr.forEach((element) => {
+    if (element > 5) {
+      emptyArr.push(element);
+    }
+  });
+
+  emptyArr.forEach((element) => {
+    console.log(element);
+  });
+}
+
+function exercise4() {
+  const arr = [1, 9, 91, 9];
+  let sum = 0;
+
+  arr.forEach((element) => {
+    sum += element;
+  });
+
+  console.log(sum);
+}
+
+function exercise5() {
+  const array = [1, 23, 44, 666, 211];
+  array.forEach((element) => {
+    if (element === 666) {
+      console.log(element);
+    }
+  });
+}
+
+function exercise6() {
+  console.log("-----------------------------");
+  const arr = [1, 4, 12, 55, 22, 3];
+  arr.reverse();
+  arr.shift();
+  arr.reverse();
+  arr.forEach((element) => {
+    console.log(element);
+  });
+}
+
+exercise6();
